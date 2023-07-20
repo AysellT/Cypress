b337bdf821eaefa82c709e27839798a9956227cb
AysellT <aysltrg3@gmail.com>
2023-07-20 08:29:54 +0300

    api assert

--- cypress/e2e/0-MyCypress/day09/d09_T04_DummyDELETE.cy.js
+++ cypress/e2e/0-MyCypress/day09/d09_T04_DummyDELETE.cy.js
@@ -2,7 +2,7 @@ describe('DELETE', () => {
 
     it('', () => {
         cy.request({
-            url:"https://dummy.restapiexample.com/api/v1/delete/2",
+            url:"https://dummy.restapiexample.com/api/v1/delete/5",
             method:'DELETE'
         }).then(response =>{
             expect(response.body.message).to.eq('Successfully! Record has been deleted')
